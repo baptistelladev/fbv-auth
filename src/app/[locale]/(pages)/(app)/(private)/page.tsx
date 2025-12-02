8344f4318f19fe0df85f9c992cfab0b7afcc1cd0
Felipe Baptistella <vbf.digi@gmail.com>
2025-12-02 00:22:21 -0300

    :art: remoção da private

--- src/app/[locale]/(pages)/(app)/(private)/page.tsx
+++
@@ -1,9 +0,0 @@
-import { LogoComp } from "@/components/custom/logo";
-
-export default function InicioPage() {
-  return (
-    <section className="inicio-page-wrapper">
-      <LogoComp classNames="text-2xl" href="/" />
-    </section>
-  );
-}
